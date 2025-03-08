7d3297fa5df26bae1c69abcc9f33e6a3d69dffaa
Jace Rockman <jacemaverick5@gmail.com>
2025-03-07 23:27:57 -0600

    Add indentation to sub-nodes

--- src/main.cljs
+++ src/main.cljs
@@ -20,41 +20,45 @@
 
 (defn text-node-component
   [{:keys [primary-sub-node sub-nodes
-           text-value]}]
+           text-value]}
+   nesting-depth]
   [:> rn/View {:key   (str (random-uuid))
-               :style {:width "100%" :background :gray}}
+               :style {:width "100%" :background :gray :margin-left (* 10 nesting-depth)}}
    [:> rn/Text text-value]
    (when sub-nodes
-     (map render-node sub-nodes))])
+     (map render-node sub-nodes (repeat (inc nesting-depth))))])
 
 (defn time-node-component
   [{:keys [primary-sub-node sub-nodes 
            start-time end-time
-           on-time-start on-time-end]}]
+           on-time-start on-time-end]}
+   nesting-depth]
   [:> rn/View {:key   (str (random-uuid))
-               :style {:width "100%" :background :gray}}
+               :style {:width "100%" :background :gray :margin-left (* 10 nesting-depth)}}
    [:> rn/Text (str "Time Value: " start-time)]
    (when sub-nodes
-     (map render-node sub-nodes))])
+     (map render-node sub-nodes (repeat (inc nesting-depth))))])
 
 (defn task-node-component
   [{:keys [primary-sub-node sub-nodes 
-           task-value on-task-toggled]}]
+           task-value on-task-toggled]}
+   nesting-depth]
   [:> rn/View {:key   (str (random-uuid))
-               :style {:width "100%" :background :gray}}
+               :style {:width "100%" :background :gray :margin-left (* 10 nesting-depth)}}
    [:> rn/Text (str "Task Value: " task-value)]
    (when sub-nodes
-     (map render-node sub-nodes))])
+     (map render-node sub-nodes (repeat (inc nesting-depth))))])
 
 (defn tracker-node-component
   [{:keys [primary-sub-node sub-nodes
            tracker-value tracker-max-value
-           on-tracker-increase on-tracker-decrease]}]
+           on-tracker-increase on-tracker-decrease]}
+   nesting-depth]
   [:> rn/View {:key   (str (random-uuid))
-               :style {:width "100%" :background :gray}}
+               :style {:width "100%" :background :gray :margin-left (* 10 nesting-depth)}}
    [:> rn/Text (str "Tracker Value: " tracker-value)]
    (when sub-nodes
-     (map render-node sub-nodes))])
+     (map render-node sub-nodes (repeat (inc nesting-depth))))])
 
 (defn determine-node-function
   [{:keys [text-value start-time task-value tracker-value] :as node}]
@@ -67,21 +71,20 @@
     :else                 println))
 
 (defn render-node
-  [node-data]
+  [node-data nesting-depth]
   (let [node-fn (determine-node-function node-data)]
-    (node-fn node-data)))
+    (node-fn node-data nesting-depth)))
 
 (defn example-group-component
   [conn]
   (let [ex-data (ffirst (ds/q '[:find (pull ?e [*])
                                            :where [?e :text-value "ProductiviT"]]
                                          @conn))]
-    (println ex-data)
     [:> rn/View
      [:> rn/Text (:text-value ex-data)]
      [:> rn/Text (str (:primary-sub-node ex-data))]
      (doall (map raw-text-node (:sub-nodes ex-data)))
-     (render-node ex-data)]))
+     (render-node ex-data 0)]))
 
 (defn root [conn]
   (let [main-nav nil #_(when (not (nil? conn)) (navigation/get-main-nav-state conn))]
