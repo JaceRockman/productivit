b4afaf6329b1ac05c45e359b2f66c6fff4bbbf25
Jace Rockman <jacemaverick5@gmail.com>
2025-04-09 03:34:01 -0500

    Implement placeholder action menu for nodes

--- src/ui/node.cljs
+++ src/ui/node.cljs
@@ -37,51 +37,73 @@
       (* 42 (count-subnodes (:sub-nodes node-data)))
       0)))
 
-(defn animated-node
-  [state-conn db-conn render-content node-data nesting-depth child-nodes]
-  (let [entity-id (:db/id node-data)
-        get-height-val (fn []
-                         (get-component-state state-conn entity-id :height-val))
-        update-height-val (fn [new-height]
-                            (ds/transact! state-conn [{:db/id entity-id :height-val new-height}]))
-        update-component-state (fn [state-conn entity-id attribute value]
-                                 (ds/transact! state-conn [{:db/id entity-id attribute value}]))
+(defn get-height-val
+  [state-conn entity-id]
+  (get-component-state state-conn entity-id :height-val))
 
-        _ (when (nil? (get-component-state state-conn entity-id :show-children))
-            (update-component-state state-conn entity-id :show-children true))
+(defn update-height-val
+  [state-conn entity-id new-height]
+  (ds/transact! state-conn [{:db/id entity-id :height-val new-height}]))
 
-        ;; Initialize once on first render
-        _ (when (nil? (get-height-val))
-            (let [is-expanded (get-component-state state-conn entity-id :show-children)
-                  initial-height (if is-expanded (calculate-height node-data) 0)]
+(defn update-component-state
+  [state-conn entity-id attribute value]
+  (ds/transact! state-conn [{:db/id entity-id attribute value}]))
 
-              (update-height-val (new (.-Value rn/Animated) initial-height))))
-        
-        ;; Toggle function
-        toggle (fn []
-                (let [new-expanded (not (get-component-state state-conn entity-id :show-children))
-                      _ (update-component-state state-conn entity-id :show-children new-expanded)
-                      to-value (if new-expanded
-                                 (calculate-height node-data)
-                                 0)]
-                                    
-                  (println "Starting animation from" (.-value (get-height-val)) "to" to-value "over 300ms")
+(defn toggle 
+    [state-conn node-data]
+    (fn []
+        (let [entity-id (:db/id node-data)
+              new-expanded (not (get-component-state state-conn entity-id :show-children))
+              _ (update-component-state state-conn entity-id :show-children new-expanded)
+              to-value (if new-expanded
+                        (calculate-height node-data)
+                        0)]
                   
-                  ;; Direct JavaScript interop approach
                   (let [animation (.timing rn/Animated 
-                                          (get-height-val) 
+                                          (get-height-val state-conn entity-id) 
                                           #js {:toValue to-value
                                               :duration 300
                                               :useNativeDriver false})]
                     (.start animation (fn [finished]
-                                        (println "Animation finished:" finished))))))]
+                                        (println "Animation finished:" finished)))))))
+
+(defn ActionMenu [node-data]
+  [:> rn/View 
+    [:> rn/View {:style {:background-color :white :flex-direction :row}}
+        [:> rn/Button {:title "Option 1" :on-press #(println "Option 1 selected")}]
+        [:> rn/Button {:title "Option 2" :on-press #(println "Option 2 selected")}]]
+   (divider)])
+
+(defn toggle-menu
+  [state-conn db-conn node-data]
+  (fn []
+    (let [entity-id (:db/id node-data)
+          new-show-menu (not (get-component-state state-conn entity-id :show-menu))]
+      (ds/transact! state-conn [{:db/id entity-id :show-menu new-show-menu}]))))
+
+(defn animated-node
+  [state-conn db-conn render-content node-data nesting-depth child-nodes]
+  (let [entity-id (:db/id node-data)
+        
+        _ (when (nil? (get-component-state state-conn entity-id :show-children))
+            (update-component-state state-conn entity-id :show-children true))
+
+        ;; Initialize once on first render
+        _ (when (nil? (get-height-val state-conn entity-id))
+            (let [is-expanded (get-component-state state-conn entity-id :show-children)
+                  initial-height (if is-expanded (calculate-height node-data) 0)]
+
+              (update-height-val state-conn entity-id (new (.-Value rn/Animated) initial-height))))]
     
     [:> rn/View
-     [:> rn/Pressable {:on-press toggle}
+     [:> rn/Pressable {:on-press (toggle state-conn node-data)
+                      :on-long-press (toggle-menu state-conn db-conn node-data)}
       (render-content state-conn node-data nesting-depth)]
      (divider)
+     (when (get-component-state state-conn entity-id :show-menu)
+       (ActionMenu node-data))
      [:> rn/Animated.View 
       {:style {:overflow "hidden"
-               :height (get-height-val)}}
+               :height (get-height-val state-conn entity-id)}}
       ;; Always render sub-nodes, but they'll be hidden when height is 0
       child-nodes]]))
